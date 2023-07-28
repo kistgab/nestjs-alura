e929e6172a5039e108e79200a8753a70c7ec0484
Gabriel Kist <gabrielkist02@gmail.com>
2023-07-28 11:51:27 -0300

    feat(database): database connection with postgres container

--- shop-project/src/app.module.ts
+++ shop-project/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UserModule, ProductsModule],
+  imports: [
+    UserModule,
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
