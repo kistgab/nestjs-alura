a2a5d0c5f654d05f9e239ccb719dc280def6d208
Gabriel Kist <gabrielkist02@gmail.com>
2023-07-31 09:08:01 -0300

    feat(productEntity): added decorators and entity in database config

--- shop-project/src/products/product.entity.ts
+++ shop-project/src/products/product.entity.ts
@@ -1,14 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { ProductCharacteristicDTO } from './dto/characteristic.dto';
 import { ProductImageDTO } from './dto/image.dto';
 
+@Entity({ name: 'products' })
 export class ProductEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column({ name: 'user_id', length: 100, nullable: false })
   userId: number;
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+  @Column({ name: 'price', nullable: false })
   price: number;
+  @Column({ name: 'disponible_quantity', nullable: false })
   disponibleQuantity: 10;
+  @Column({ name: 'description', length: 255, nullable: false })
   description: string;
-  characteristics: ProductCharacteristicDTO[];
-  images: ProductImageDTO[];
+  // characteristics: ProductCharacteristicDTO[];
+  // images: ProductImageDTO[];
+  @Column({ name: 'category', length: 100, nullable: false })
   category: string;
 }
