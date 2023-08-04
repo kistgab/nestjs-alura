bcc252ed33e56b59042807cafa8eb2c9f1dcaa7a
Gabriel Kist <gabrielkist02@gmail.com>
2023-08-04 11:51:59 -0300

    feat: starting migrations

--- shop-project/src/db/data-source-cli.ts
+++ shop-project/src/db/data-source-cli.ts
@@ -8,8 +8,8 @@ const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + '/../**/*.entity{.js,.ts}'],
-  migrations: [__dirname + '/migrations/*.{js,.ts}'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  migrations: [__dirname + '/migrations/*.{js,ts}'],
 };
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
