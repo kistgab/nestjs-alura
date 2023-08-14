30a64d6272e2ed06f308c9bf0b7bfc9477b6d08f
Gabriel Kist <gabrielkist02@gmail.com>
2023-08-14 11:35:37 -0300

    refactor(updateDTOs): using mapped types in the DTOs

--- shop-project/src/user/dto/update-user.dto.ts
+++ shop-project/src/user/dto/update-user.dto.ts
@@ -1,17 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { UniqueEmail } from '../validations/unique-email.validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDTO } from './create-user.dto';
 
-export class UpdateUserDTO {
-  @IsOptional()
-  @IsNotEmpty({ message: 'O nome não pode ser vazio!' })
-  name: string;
-
-  @IsOptional()
-  @IsEmail(undefined, { message: 'O e-mail deve ser válido!' })
-  @UniqueEmail({ message: 'Já existe um usuário com este e-mail' })
-  email: string;
-
-  @IsOptional()
-  @MinLength(6, { message: 'A Senha deve ter pelo menos 6 caracteres' })
-  password: string;
-}
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {}
